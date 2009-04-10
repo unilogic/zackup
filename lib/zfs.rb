99aefca2c59d9fe3dd46b5eac1d385b2138f8679
Ben Allen <ben@sysadminschronicles.com>
2009-04-10 01:10:42 -0400

    Testing 3

--- lib/zfs.rb
+++ lib/zfs.rb
@@ -1,5 +1,6 @@
 module Zfs
   
+  
   #
   # Input: args = {"flags" => "fn", "pool" => "test_pool", "properties" => { "prop1" => "value1" },"vdev" => "/dev/test"}
   # Returns: [Exit status, Command Output]
