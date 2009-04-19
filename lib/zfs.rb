e981839573ff3207b60f234f22d8dd86e811f81a
Ben Allen <ben@sysadminschronicles.com>
2009-04-19 01:31:04 -0400

    Added zfs_list

--- lib/zfs.rb
+++ lib/zfs.rb
@@ -23,13 +23,13 @@ module Zfs
     if args["pool"]
       arglist << " #{args["pool"]}"
     else
-      return 1
+      return 1, "Pool must be defined"
     end
     
     if args["vdev"]
       arglist << " #{args["vdev"]}"
     else
-      return 1
+      return 1, "Vdev must be defined"
     end
     result = %x[zpool create#{arglist} 2>&1]
     
@@ -63,12 +63,15 @@ module Zfs
   def zpool_offline
     
   end
+  
   def zpool_clear
     
   end
+  
   def zpool_attach
     
   end
+  
   def zpool_detach
     
   end
@@ -123,7 +126,7 @@ module Zfs
     end
     
     if args["filesystem"] && ( args["size"] || args["volume"] )
-      return 1
+      return 1, "Filesystem and size or volume cannot be defined at the same time"
     elsif args["filesystem"] && !( args["size"] || args["volume"] )
       arglist << properties
       arglist << " #{args["filesystem"]}"
@@ -135,7 +138,7 @@ module Zfs
       arglist << " -V #{args["size"]}"
       arglist << " #{args["volume"]}"
     else
-      return 1
+      return 1, "Filesystem or size and volume must be defined"
     end
 
     result = %x[zfs create#{arglist} 2>&1]
@@ -143,31 +146,107 @@ module Zfs
     return $?.exitstatus,result
   end
   
-  def zfs_destroy
+  # args = {"flags" => "rRf", "filesystem" => "/pool/folder", volume => "pool", "snapshot" => "snapName"}
+  def zfs_destroy(args)
+    arglist = ""
+    if args["flags"]
+      arglist << "-#{args["flags"]}"
+    end
     
-  end
-  def zfs_snapshot
+    if args["filesystem"]
+      arglist << " #{args["filesystem"]}"
+    elsif args["snapshot"]
+      arglist << " #{args["snapshot"]}"
+    elsif args["volume"]
+      arglist << " #{args["volume"]}"
+    else
+      return 1, "Filesystem, snapshot or volume must be defined"
+    end
+    result = %x[zfs destroy#{arglist} 2>&1]
     
+    return $?.exitstatus,result
   end
   
-  def zfs_rollback
+  #args = {"flags" => "r", "filesystem" => "/pool/folder", volume => "pool", "snapname" => "name"}
+  def zfs_snapshot(args)
+    arglist = ""
+    if args["flags"]
+      arglist << "-#{args["flags"]}"
+    end
+    if ! args["snapname"]
+      return 1, "Snapname must be defined"
+    elsif args["filesystem"] && args["volume"]
+      return 1, "Filesystem and volume cannot be defined at the same time"
+    elsif args["filesystem"]
+      arglist << " #{args["filesystem"]}@#{args["snapname"]}"
+    elsif args["volume"]
+      arglist << " #{args["volume"]}@#{args["snapname"]}"
+    else
+      return 1, "Snapname and volume or filesystem must be defined"
+    end
+    result = %x[zfs snapshot#{arglist} 2>&1]
     
+    return $?.exitstatus,result
   end
   
-  def zfs_clone
+  # args = {"flags" => "rRf", "snapshot"}
+  def zfs_rollback(args)
+    arglist = ""
+    if args["flags"]
+      arglist << "-#{args["flags"]}"
+    end
+
+    if args["snapshot"]
+      arglist << " #{args["snapshot"]}"
+    else
+      return 1, "Snapshot must be defined"
+    end
+    result = %x[zfs rollback#{arglist} 2>&1]
     
+    return $?.exitstatus,result
   end
   
-  def zfs_promote
+  def zfs_clone(args)
+    arglist = ""
+    if args["flags"]
+      arglist << "-#{args["flags"]}"
+    end
+    if args["snapshot"]
+      arglist << " #{args["snapshot"]}"
+    elsif args["filesystem"] && args["volume"]
+      return 1, "Filesystem and volume cannot be defined at the same time"
+    elsif args["filesystem"]
+      arglist << " #{args["filesystem"]}"
+    elsif args["volume"]
+      arglist << " #{args["volume"]}"
+    else
+      return 1, "Snapshot and volume or filesystem must be defined"
+    end
+    result = %x[zfs clone#{arglist} 2>&1]
     
+    return $?.exitstatus,result
+  end
+  
+  # args = {"clone-filesystem" => "file/system"}
+  def zfs_promote(args)
+    arglist = ""
+    if args["clone-filesystem"]
+      arglist << " #{args["clone-filesystem"]}"
+    else
+      return 1, "Clone-filesystem must be defined"
+    end
+    result = %x[zfs promote#{arglist} 2>&1]
+    
+    return $?.exitstatus,result
   end
   
   def zfs_rename
     
   end
-  
-  def zfs_list
     
+  def zfs_list (args)
+    result = parse_output(%x[zfs list 2>&1])
+    return $?.exitstatus,result
   end
   
   def zfs_set
@@ -216,6 +295,23 @@ module Zfs
   
   def zfs_unallow
     
+  end
+  
+  protected
+  
+  def parse_output(str)
+    lines = str.split("\n")
+    cols = lines[0].chomp.squeeze(" ").downcase!.split(" ")
+    lines.shift
+    record = []
+    lines.each do |line|
+      vals = line.chomp.squeeze(" ").split(" ")
+      record.push((0...cols.size).map {|j|
+                    { cols[j] => vals[j] }
+            }
+      )
+    end
+    return record
   end
   
 end
