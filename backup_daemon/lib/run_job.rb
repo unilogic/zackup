047c72f6e2d0356c6b6c3df4bbca1cbe0b85b630
Ben Allen <ben@sysadminschronicles.com>
2009-07-23 20:34:44 -0400

    Fix bug in user show view.

--- backup_daemon/lib/run_job.rb
+++ backup_daemon/lib/run_job.rb
@@ -1,11 +1,18 @@
+require 'setup_job'
+
 module RunJob
-  if job.operation == 'setup'
+  def run(jobs)
+    jobs.each do |job|
+      if job.operation == 'setup'
+        job.data
+        setupJob = SetupJob.new()
+      elsif job.operation == 'maintenance'
     
-  elsif job.operation == 'maintenance'
+      elsif job.operation == 'backup'
     
-  elsif job.operation == 'backup'
+      elsif job.operation == 'restore'
     
-  elsif job.operation == 'restore'
-    
+      end
+    end
   end
 end
