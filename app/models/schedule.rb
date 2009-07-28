d835e9b7fea3faa668e8958c31aa4166aa947806
Ben Allen <ben@sysadminschronicles.com>
2009-07-27 22:19:14 -0400

    Added dependancy destroy on schedule -> job has_many.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,8 +1,8 @@
 class Schedule < ActiveRecord::Base
   belongs_to :host
   belongs_to :backup_node, :class_name => 'Node'
-  has_one :retention_policy
-  has_many :jobs
+  has_one :retention_policy, :dependent => :destroy
+  has_many :jobs, :dependent => :destroy
   
   #validates_uniqueness_of :backup_node_id, :scope => :host_id, :message => ", a schedule for this host already exists on the selected Backup Node"
 end
