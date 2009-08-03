9f89319d06fa765760cbd03d058cffbd9ebb4ef9
Ben Allen <ben@sysadminschronicles.com>
2009-08-03 15:32:47 -0400

    First test FileIndex get_content.

--- app/models/file_index.rb
+++ app/models/file_index.rb
@@ -8,16 +8,39 @@ class FileIndex < ActiveRecord::Base
     YAML::load(Zlib::Inflate.inflate(self.data))
   end
   
-  def get_dirs(path=".", data=self.data)
+  def get_dirs(path="", data)
      
   end
   
-  def get_files(path=".")
+  def get_files(path="", data=self.data)
     
   end
   
-  def get_content(path=".")
-      [get_dirs(path), get_files(path)]
+  def get_content(path="", data_inflate=self.data_inflate)
+    hash_arg = ""
+    files = []
+    dirs = []
+    if path.length == 0
+      hash_arg = "['#{self.snapname}']"
+    else
+      hash_arg = "['#{self.snapname}']"
+      path.split('/').each do |path_part|
+        if path_part.length > 0
+          hash_arg << "['#{path_part}']"
+        end
+      end
+    end
+    path_contents = eval("data_inflate#{hash_arg}.keys")
+    path_contents.each do |content|
+      sub_content = eval("data_inflate#{hash_arg}['#{content}'].keys")
+      if sub_content.length > 0
+        dirs << content
+      else
+        files << content
+      end
+    end
+    
+    return [dirs, files]
   end
     
 end
