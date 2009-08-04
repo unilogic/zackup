58ed917c4a1cf187a80a75dee68e082c3b97ac58
Ben Allen <ben@sysadminschronicles.com>
2009-08-04 01:11:34 -0400

    More.

--- app/controllers/restores_controller.rb
+++ app/controllers/restores_controller.rb
@@ -2,5 +2,6 @@ class RestoresController < ApplicationController
   def new
     @host = Host.find(params[:host_id])
     @restore = Restore.new(:host_id => params[:host_id])
+    @schedules = @host.schedules
   end
 end
