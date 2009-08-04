77e200adb52c7cf88cd7e02c21a9dd9a254105e2
Ben Allen <ben@sysadminschronicles.com>
2009-08-04 15:48:49 -0400

    Whoops.

--- app/controllers/file_indices_controller.rb
+++ app/controllers/file_indices_controller.rb
@@ -17,6 +17,7 @@ class FileIndicesController < ApplicationController
     @remote_id = params[:remote_id]
     
     @file_index = FileIndex.find(params[:id])
+    @file_indices = FileIndex.find_all_by_host_id_and_schedule_id(params[:host_id], params[:schedule_id], :select => "id,snapname")
     
     @current_dir = params[:dir] || ""
     @current_dir.gsub!(/^\/*/, "/")
