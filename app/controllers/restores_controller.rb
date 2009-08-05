74d54dc7d66b63db246c97a2cb732d31ccce967e
Ben Allen <ben@sysadminschronicles.com>
2009-08-05 13:09:56 -0400

    Last.

--- app/controllers/restores_controller.rb
+++ app/controllers/restores_controller.rb
@@ -20,11 +20,12 @@ class RestoresController < ApplicationController
     @restore = Restore.new(params[:restore])
     @restore.host_id = params[:host_id]
     @restore.schedule_id = params[:schedule][:schedule_id]
+    
     file_indices = FileIndex.find_all_by_host_id_and_schedule_id(params[:host_id], params[:schedule][:schedule_id], :select => "id,snapname")
-    if file_indices && @restore.save!
+    if file_indices.length > 0 && @restore.save!
       redirect_to host_restore_schedule_file_indices_path(params[:host_id], @restore.id, params[:schedule][:schedule_id])
     else
-      flash[:error] = "Had a problem creating the restore. Either there are no snapshots for this host and schedule or there was a problem with the database!"
+      flash[:error] = "Problem creating the restore. Either there are no snapshots for this host and schedule or there was a problem with the database!"
       @host = Host.find(params[:host_id])
       @restore = Restore.new
       @schedules = @host.schedules
