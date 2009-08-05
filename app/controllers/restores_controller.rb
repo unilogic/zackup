bd5f84cea09d4eb929a5eb689323b105a27a89be
Ben Allen <ben@sysadminschronicles.com>
2009-08-05 13:04:41 -0400

    Tweaks in restores controller.

--- app/controllers/restores_controller.rb
+++ app/controllers/restores_controller.rb
@@ -6,6 +6,7 @@ class RestoresController < ApplicationController
   end
   
   def show
+    @host = Host.find(params[:host_id])
     @restore = Restore.find(params[:id])
   end
   
@@ -19,13 +20,26 @@ class RestoresController < ApplicationController
     @restore = Restore.new(params[:restore])
     @restore.host_id = params[:host_id]
     @restore.schedule_id = params[:schedule][:schedule_id]
-    
-    if @restore.save!
+    file_indices = FileIndex.find_all_by_host_id_and_schedule_id(params[:host_id], params[:schedule][:schedule_id], :select => "id,snapname")
+    if file_indices && @restore.save!
       redirect_to host_restore_schedule_file_indices_path(params[:host_id], @restore.id, params[:schedule][:schedule_id])
     else
+      flash[:error] = "Had a problem creating the restore. Either there are no snapshots for this host and schedule or there was a problem with the database!"
       @host = Host.find(params[:host_id])
+      @restore = Restore.new
+      @schedules = @host.schedules
       render :action => :new
     end
   end
   
+  def destroy
+    @restore = Restore.find(params[:id])
+    if request.delete?
+      flash[:notice] = "Restore deleted!"
+      @restore.destroy
+    end
+
+    redirect_to host_restores_path(params[:host_id])
+
+  end
 end
