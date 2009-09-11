da4cc62e70bce239ab2a855115d71e81b56fa512
Ben Allen <ben@unilogiclabs.com>
2009-09-11 16:10:17 -0400

    Added chartr (flotr) version.

---
+++ vendor/plugins/chartr/lib/chartrfiles.rb
@@ -0,0 +1,9 @@
+chartr_path = RAILS_ROOT + "/vendor/plugins/chartr"
+flotr = "flotr/release/prototype/flotr-0.2.0-test/flotr"
+
+  # Files to be copied
+ChartrFiles = ["#{chartr_path}/#{flotr}/flotr.debug-0.2.0-test.js",
+               "#{chartr_path}/#{flotr}/lib/canvastext.js",
+               "#{chartr_path}/#{flotr}/lib/excanvas.js",
+               "#{chartr_path}/#{flotr}/lib/base64.js",
+               "#{chartr_path}/#{flotr}/lib/canvas2image.js"]
