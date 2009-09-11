6702231d6692c883d3ed5adb30ea82abea73cb32
Ben Allen <ben@unilogiclabs.com>
2009-09-10 20:15:09 -0400

    Filled out stats migration.

--- db/migrate/20090910235531_create_stats.rb
+++ db/migrate/20090910235531_create_stats.rb
@@ -1,7 +1,15 @@
 class CreateStats < ActiveRecord::Migration
   def self.up
+    create_table :stats do |t|
+      t.integer :schedule_id
+      t.integer :node_id
+      t.string :disk_used
+      t.string :disk_avail
+      t.string :cpu_load_avg
+    end
   end
 
   def self.down
+    drop_table :stats
   end
 end
